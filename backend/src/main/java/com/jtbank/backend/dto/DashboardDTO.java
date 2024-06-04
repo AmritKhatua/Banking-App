24e038cbdaa646a79e101ad616efa13ef353f0a4
AmritKhatua <khatuaa849@gmail.com>
2024-06-04 22:23:02 +0530

    bank

---
+++ backend/src/main/java/com/jtbank/backend/dto/DashboardDTO.java
@@ -0,0 +1,14 @@
+package com.jtbank.backend.dto;
+
+import java.util.List;
+
+public record DashboardDTO(
+        long numberOfDeposit,
+        long numberOfWithdrawal,
+        long numberOfTransfer,
+        List<TransactionDTO> transactions
+) {
+}
+
+
+//front end 
